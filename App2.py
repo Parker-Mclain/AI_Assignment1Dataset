a489301feb2a44f32e5a4efe3b243c80017082fb
Parker Mclain <parkermclain94@gmail.com>
2018-09-17 21:48:54 -0400

    Both A and B plots using same data

--- App2.py
+++ App2.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
 
 # Average male height (US): 176.4 cm
 # Average female height (US): 162.9 cm
@@ -61,34 +62,46 @@ plt.legend(loc='upper left')
 plt.xlabel('Weight (lb)')
 plt.ylabel('Height (cm)')
 
+maleHeightData = np.zeros((2000,), dtype=float)
+femaleHeightData = np.zeros((2000,), dtype=float)
+for i in range(len(maleData[0])):
+    maleHeightData[i] = maleData[1][i]
+for i in range(len(femaleData[0])):
+    femaleHeightData[i] = femaleData[1][i]
+
 # *******************
 # Histogram Plot Datasets for part A (Only height)
 # *** UNCOMMENT THIS SECTION AND COMMENT OUT THE "LINE PLOT" TO VIEW ***
 # *******************
-# plt.subplot(212)
-# maleData = np.random.normal(maleMean[1], maleSTD[1], 2000)
-# femaleData = np.random.normal(femaleMean[1], femaleSTD[1], 2000)
-# plt.hist(maleData, bins=np.arange(maleData.min(), maleData.max()+1), label='Male')
-# plt.hist(femaleData, bins=np.arange(femaleData.min(), femaleData.max()+1), label='Female')
-# plt.legend(loc='upper left')
-# plt.xlabel('Height (cm)')
-# plt.ylabel('Frequency')
-# plt.show()
+plt.subplot(212)
+maleData = np.random.normal(maleMean[1], maleSTD[1], 2000)
+femaleData = np.random.normal(femaleMean[1], femaleSTD[1], 2000)
+plt.hist(maleHeightData, bins=np.arange(maleHeightData.min(), maleHeightData.max()+1), label='Male')
+plt.hist(femaleHeightData, bins=np.arange(femaleHeightData.min(), femaleHeightData.max()+1), label='Female')
+plt.legend(loc='upper left')
+plt.xlabel('Height (cm)')
+plt.ylabel('Frequency')
+plt.show()
 
 # *******************
 # Line Plot Datasets for part A (Only height)
 # *** COMMENT OUT THIS SECTION AND UNCOMMENT THE "Histogram PLOT" TO VIEW ***
 # *******************
-plt.subplot(212)
-temp = np.zeros((2000,), dtype=int)
-maleData = np.random.normal(maleMean[1], maleSTD[1], 2000)
-femaleData = np.random.normal(femaleMean[1], femaleSTD[1], 2000)
-plt.plot(maleData, temp, marker='o', markersize=2, label='Male', color='blue')
-plt.plot(femaleData, temp, marker='o', markersize=1, label='Female', color='green')
-plt.axis([125, 215, -.5, .5])
-plt.legend(loc='upper left')
-plt.xlabel('Height (cm)')
-plt.show()
+# plt.subplot(212)
+# temp = np.zeros((2000,), dtype=int)
+# plt.plot(maleHeightData, temp, marker='o', markersize=2, label='Male', color='blue')
+# plt.plot(femaleHeightData, temp, marker='o', markersize=1, label='Female', color='green')
+# plt.axis([125, 215, -.5, .5])
+# plt.legend(loc='upper left')
+# plt.xlabel('Height (cm)')
+# plt.show()
+
+print("Completed at:", datetime.datetime.now().time())
+
+
+
+
+
 
 
 
